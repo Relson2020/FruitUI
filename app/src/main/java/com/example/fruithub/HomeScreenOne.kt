b0984207fc6bc018d4c8abe7165b2727dff233d7
Relson2020 <sonuprajapati@relinns.com>
2022-01-28 18:38:14 +0530

    fix the buttons sizes fragments views , addToBasket fragment .

--- app/src/main/java/com/example/fruithub/HomeScreenOne.kt
+++ app/src/main/java/com/example/fruithub/HomeScreenOne.kt
@@ -1,11 +1,11 @@
 package com.example.fruithub
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.fruithub.databinding.FragmentHomeScreenOneBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -14,9 +14,25 @@ class HomeScreenOne : Fragment() {
 
     private lateinit var binding: FragmentHomeScreenOneBinding
 
+    // button name array
     private var buttonArray: ArrayList<String> =
         arrayListOf("All", "Salad Combo", "Berry Combo", "Mango Combo")
 
+    // salad name array
+    private var recyclerView1saladName: ArrayList<String> =
+        arrayListOf("Honey lime combo", "Berry mango combo")
+
+    // salad image array
+    private var recyclerView1saladImage: ArrayList<Int> =
+        arrayListOf(R.drawable.temp, R.drawable.berry_mango)
+
+    // price image array
+    private var recyclerView1priceImage: ArrayList<Int> =
+        arrayListOf(R.drawable.ic___2000, R.drawable.ic___2000)
+
+    // cardView background color array
+    private val recyclerView1cardColor: ArrayList<Int> =
+        arrayListOf(R.color.white, R.color.white)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,22 +51,21 @@ class HomeScreenOne : Fragment() {
         // recycler view for button adapter
         binding.buttonRecyclerView.adapter = RecyclerButtonAdapter(buttonArray)
 
-        // first tabLayout viewPager
-        binding.firstViewpager.adapter = FirstViewPager(parentFragmentManager,lifecycle)
+        // recommended recycler view
+        binding.recommendedRecyclerView.adapter = RecyclerCardImageAdapter(
+            recyclerView1saladName,
+            recyclerView1saladImage,
+            recyclerView1priceImage,
+            recyclerView1cardColor
+        )
 
-        // first tabLayout
-        TabLayoutMediator(binding.firstTabLayout,binding.firstViewpager){ tab ,position ->
-            when(position){
-                0 -> tab.text = "Recommended Combo"
-            }
-        }.attach()
 
         // second tabLayout viewpager
-        binding.secondViewpager.adapter = SecondViewPager(parentFragmentManager,lifecycle)
+        binding.secondViewpager.adapter = SecondViewPager(parentFragmentManager, lifecycle)
 
         // second tabLayout
-        TabLayoutMediator(binding.secondTabLayout,binding.secondViewpager){tab,position ->
-            when(position){
+        TabLayoutMediator(binding.secondTabLayout, binding.secondViewpager) { tab, position ->
+            when (position) {
                 0 -> tab.text = "Hottest"
                 1 -> tab.text = "Popular"
                 2 -> tab.text = "New Combo"
@@ -60,7 +75,7 @@ class HomeScreenOne : Fragment() {
 
         // navigating to myBasket fragment
         binding.basketCardView.setOnClickListener {
-            it.findNavController().navigate(R.id.action_homeScreenOne_to_myBasket)
+            it.findNavController().navigate(R.id.action_homeScreenOne_to_addToBasket)
         }
     }
 }
